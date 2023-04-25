b1b9f5500b5f17323be76319683729bd26476943
JorgeBalmi <palomaclase41@gmail.com>
2023-04-25 14:25:05 +0200

    UwU

--- src/ies/paloma/ProbandoGit.java
+++ src/ies/paloma/ProbandoGit.java
@@ -6,6 +6,9 @@ public class ProbandoGit {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hola 1");
+		System.out.println("UwU");
+		System.out.println("卐");
+		System.out.println("Tu madre es una bellísima persona por parirte OwO");
 	}
 
 }
